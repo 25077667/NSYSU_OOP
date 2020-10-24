1d808272cae3ec19a2ea14b34e1ff8f5350947ec
25077667 <zxc25077667@gmail.com>
2020-10-24 16:32:42 +0800

    Fix convert string

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -6,12 +6,14 @@ int main(int argc, char *argv[])
 {
     char mode = parser(argc, argv);
     Tar myTar(std::string(argv[2]), mode);
-    for (int i = 3; i <= argc; i++) {
+    for (int i = 3; i < argc; i++) {
         try {
             myTar.operate(std::string(argv[i]));
         } catch (char const *error) {
             std::cerr << error << std::endl;
         }
     }
+    if (mode & 0b00010000)
+        myTar.operate(std::string());
     return 0;
 }
