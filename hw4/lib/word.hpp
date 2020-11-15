60e0b9842a6ec25478b6cdc72279290f6382dbef
25077667 <zxc25077667@gmail.com>
2020-11-16 00:18:56 +0800

    Add magic_enum

--- hw4/lib/word.hpp
+++ hw4/lib/word.hpp
@@ -4,11 +4,18 @@
 #ifndef WORD_HPP
 #define WORD_HPP
 #include <iostream>
+#include "magic_enum.hpp"
 #include "tag.hpp"
 #include "type.hpp"
 using namespace std;
 
-string getWord(string lexeme) {}
+string getWord(string_view lexeme, Tag tag)
+{
+    string word = string(lexeme);
+    if (!!tag)  // in the TAG
+        word = string(magic_enum::enum_name(tag));
+    return word;
+}
 
 
 #endif
