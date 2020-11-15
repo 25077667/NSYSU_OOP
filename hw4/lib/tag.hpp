d8ad70cd40e3356fe4e93ce9d948c4f44e1ba4b6
25077667 <zxc25077667@gmail.com>
2020-11-16 00:20:39 +0800

    Fix tags IO

--- hw4/lib/tag.hpp
+++ hw4/lib/tag.hpp
@@ -25,16 +25,33 @@ enum class Tag : char {
     REAL,
     TEMP,
     TRUE,
-    WHILE
+    WHILE,
+    SCOPE,
 };
 
-constexpr frozen::map<std::string, Token, 16> tagTable{
-    {"&&", Token("&&")}, {"&", Token("&")},   {"||", Token("||")},
-    {"|", Token("|")},   {"==", Token("==")}, {"=", Token("=")},
-    {"!=", Token("!=")}, {"!", Token("!")},   {"<=", Token("<=")},
-    {"<", Token("<")},   {"<<", Token("<<")}, {">=", Token(">=")},
-    {">", Token(">")},   {">>", Token(">>")}, {":", Token(":")},
-    {"::", Token("::")},
+bool operator!(Tag tag)
+{
+    return tag == static_cast<Tag>(0);
+}
+
+// The second char must offset +1, because the peek offset +1
+constexpr frozen::map<std::string_view, Token, 16> tagTable{
+    {"&'", Token("&&", Tag::AND)},
+    {"&", Token("&")},
+    {"|}", Token("||", Tag::OR)},
+    {"|", Token("|")},
+    {"=>", Token("==", Tag::EQ)},
+    {"=", Token("=")},
+    {"!>", Token("!=", Tag::NE)},
+    {"!", Token("!")},
+    {"<>", Token("<=", Tag::LE)},
+    {"<", Token("<")},
+    {"<?", Token("<<")},
+    {">>", Token(">="), Tag::GE},
+    {">", Token(">")},
+    {">?", Token(">>")},
+    {":", Token(":")},
+    {":;", Token("::"), Tag::SCOPE},
 };
 
 #endif
--- hw4/lib/token.hpp
+++ hw4/lib/token.hpp
@@ -1,24 +1,32 @@
 #ifndef TOKEN_HPP
 #define TOKEN_HPP
 #include <iostream>
+#include <string_view>
+#include "tag.hpp"
 #include "type.hpp"
 #include "word.hpp"
 
 class Token
 {
-    std::string token;
+    std::string_view token;
+    Tag tag = Tag::EMPTY;
 
 public:
     Token() = default;
-    constexpr explicit Token(std::string _token) : token(_token) {}
+    constexpr explicit Token(std::string_view _token) : token(_token) {}
+    constexpr explicit Token(std::string_view _token, Tag _tag)
+        : token(_token), tag(_tag)
+    {
+    }
     friend std::ostream &operator<<(std::ostream &_out, const Token &_token);
     Token operator=(const Token &_token);
+    operator bool() const { return !this->token.empty(); }
 };
 
 std::ostream &operator<<(std::ostream &_out, const Token &_token)
 {
-    _out << "Token: " << (_token.token) << "\t(" << getWord(_token.token)
-         << ")";
+    _out << "Token: " << (_token.token) << "\t("
+         << getWord(_token.token, _token.tag) << ")";
     return _out;
 }
 #endif
