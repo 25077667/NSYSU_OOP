9c93de52e20487a1ccddce0314fd365a2de95a8b
25077667 <zxc25077667@gmail.com>
2020-11-16 00:22:12 +0800

    Fix type declarations

--- hw4/lib/type.hpp
+++ hw4/lib/type.hpp
@@ -8,22 +8,22 @@ using namespace std;
 class Type
 {
 private:
-    string __type;
+    string_view __type;
     static constexpr array<string_view, 9> allType = {
-        {{"int", "float", "double", "char", "bool", "void", "string", "auto",
-          "enum"}}};  // class is not a type
-    static constexpr array<string_view, 16> specifier = {
-        {{"signed", "unsigned", "long", "short", "static", "extern", "const",
-          "inline", "volatile", "restrict", "public", "private", "protected",
-          "virtual", "constexpr", "explicit", "friend"}}};
+        {"int", "float", "double", "char", "bool", "void", "string_view",
+         "auto", "enum"}};  // class is not a type
+    static constexpr array<string_view, 17> specifier = {
+        {"signed", "unsigned", "long", "short", "static", "extern", "const",
+         "inline", "volatile", "restrict", "public", "private", "protected",
+         "virtual", "constexpr", "explicit", "friend"}};
 
 public:
     Type() = default;
-    Type(string _type);
+    Type(string_view _type);
     ~Type() = default;
-    string getType();
+    string_view getType();
 };
 
-Type::Type(string _type) : __type(_type) {}
+Type::Type(string_view _type) : __type(_type) {}
 
 #endif
