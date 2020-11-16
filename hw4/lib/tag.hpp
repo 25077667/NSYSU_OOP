2fbdb4f7cd90989d6a0cb8a47a39c362a998e40f
25077667 <zxc25077667@gmail.com>
2020-11-16 21:16:41 +0800

    Move tagTable to token

--- hw4/lib/tag.hpp
+++ hw4/lib/tag.hpp
@@ -1,5 +1,6 @@
 #ifndef TAG_HPP
 #define TAG_HPP
+#include <string_view>
 
 enum class Tag : char {
     EMPTY,
@@ -30,28 +31,5 @@ bool operator!(Tag tag)
 {
     return tag == static_cast<Tag>(0);
 }
-
-#include "frozen/map.h"
-#include "token.hpp"
-
-// The second char must offset +1, because the peek offset +1
-constexpr frozen::map<std::string_view, Token, 16> tagTable{
-    {string_view("&'"), Token(string_view("&&"), Tag::AND)},
-    {string_view("&"), Token(string_view("&"))},
-    {string_view("|}"), Token(string_view("||"), Tag::OR)},
-    {string_view("|"), Token(string_view("|"))},
-    {string_view("=>"), Token(string_view("=="), Tag::EQ)},
-    {string_view("="), Token(string_view("="))},
-    {string_view("!>"), Token(string_view("!="), Tag::NE)},
-    {string_view("!"), Token(string_view("!"))},
-    {string_view("<>"), Token(string_view("<="), Tag::LE)},
-    {string_view("<"), Token(string_view("<"))},
-    {string_view("<?"), Token(string_view("<<"))},
-    {string_view(">>"), Token(string_view(">="), Tag::GE)},
-    {string_view(">"), Token(string_view(">"))},
-    {string_view(">?"), Token(string_view(">>"))},
-    {string_view(":"), Token(string_view(":"))},
-    {string_view(":;"), Token(string_view("::"), Tag::SCOPE)},
-};
 
 #endif
--- hw4/lib/token.hpp
+++ hw4/lib/token.hpp
@@ -2,31 +2,58 @@
 #define TOKEN_HPP
 #include <iostream>
 #include <string_view>
+#include "frozen/map.h"
 #include "tag.hpp"
 #include "type.hpp"
 #include "word.hpp"
+using namespace std;
+
+extern string getWord(string_view lexeme, Tag tag);
 
 class Token
 {
-    std::string_view token;
+    string_view token;
     Tag tag = Tag::EMPTY;
 
 public:
     Token() = default;
-    constexpr Token(std::string_view _token) : token(_token) {}
-    constexpr Token(std::string_view _token, Tag _tag)
-        : token(_token), tag(_tag)
+    constexpr Token(string_view _token) : token(_token) {}
+    constexpr Token(string_view _token, Tag _tag) : token(_token), tag(_tag) {}
+    friend ostream &operator<<(ostream &_out, const Token &_token);
+    constexpr Token operator=(const Token &_token)
     {
+        this->token = _token.token;
+        this->tag = _token.tag;
+        return *this;
     }
-    friend std::ostream &operator<<(std::ostream &_out, const Token &_token);
-    constexpr Token operator=(const Token &_token) { this->token = _token.token; this->tag = _token.tag; return *this; }
     operator bool() const { return !this->token.empty(); }
 };
 
-std::ostream &operator<<(std::ostream &_out, const Token &_token)
+ostream &operator<<(ostream &_out, const Token &_token)
 {
     _out << "Token: " << (_token.token) << "\t("
          << getWord(_token.token, _token.tag) << ")";
     return _out;
 }
+
+// The second char must offset +1, because the peek offset +1
+constexpr frozen::map<std::string_view, Token, 16> tagTable{
+    {std::string_view("&'"), Token(std::string_view("&&"), Tag::AND)},
+    {std::string_view("&"), Token(std::string_view("&"))},
+    {std::string_view("|}"), Token(std::string_view("||"), Tag::OR)},
+    {std::string_view("|"), Token(std::string_view("|"))},
+    {std::string_view("=>"), Token(std::string_view("=="), Tag::EQ)},
+    {std::string_view("="), Token(std::string_view("="))},
+    {std::string_view("!>"), Token(std::string_view("!="), Tag::NE)},
+    {std::string_view("!"), Token(std::string_view("!"))},
+    {std::string_view("<>"), Token(std::string_view("<="), Tag::LE)},
+    {std::string_view("<"), Token(std::string_view("<"))},
+    {std::string_view("<?"), Token(std::string_view("<<"))},
+    {std::string_view(">>"), Token(std::string_view(">="), Tag::GE)},
+    {std::string_view(">"), Token(std::string_view(">"))},
+    {std::string_view(">?"), Token(std::string_view(">>"))},
+    {std::string_view(":"), Token(std::string_view(":"))},
+    {std::string_view(":;"), Token(std::string_view("::"), Tag::SCOPE)},
+};
+
 #endif
