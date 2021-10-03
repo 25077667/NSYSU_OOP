3f784e3effc0e0d100f737b173cf27bc6a6955f8
25077667 <zxc25077667@gmail.com>
2021-10-04 04:03:09 +0800

    Use unnamed-namespace qualifier to call C in C++

--- hw1/src/c++/stack.cpp
+++ hw1/src/c++/stack.cpp
@@ -1,25 +1,22 @@
 #include "stack.h"
 #include "../../src/c/stack.h"
 
-void (*push_c)(struct stack *, int) = push;
-int (*pop_c)(struct stack *) = pop;
-
 Stack::Stack()
 {
-    this->my_stack = new_stack();
+    this->my_stack = ::new_stack();
 }
 
 Stack::~Stack()
 {
-    delete_stack(this->my_stack);
+    ::delete_stack(this->my_stack);
 }
 
 void Stack::push(int x)
 {
-    push_c(this->my_stack, x);
+    ::push(this->my_stack, x);
 }
 
 int Stack::pop()
 {
-    return pop_c(this->my_stack);
+    return ::pop(this->my_stack);
 }
